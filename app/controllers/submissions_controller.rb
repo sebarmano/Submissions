739c344a8013a8de70bfadf669976ed93c6ad4a2
Sebastian Armano <sebarmano@gmail.com>
2014-06-26 00:26:30 -0400

    Added styling to students assignmnent view

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -15,7 +15,7 @@ class SubmissionsController < ApplicationController
     if @submission.save
       redirect_to assignments_path, notice: 'Assignment was successfully submitted for review.'
     else
-      render assignment_path(@assignment), notice: 'Assigned could not be submitted, try again later.'
+      redirect_to assignment_path(@assignment)
     end
   end
 
